3932e84911a6c7df689a6a866b93a6a97b2f286d
Edna0 <ojiedna@gmail.com>
2022-03-11 03:27:27 -0800

    Print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art"
- * return : 0 if exited properly, non-zero if otherwise
+ * return: 0 if exited properly, non-zero if otherwise
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
