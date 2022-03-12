b2570f091af8835f81516ce749dd30bf8d66e096
Edna0 <ojiedna@gmail.com>
2022-03-12 05:10:42 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types on the computer it is compiled and run on
- * return : 0 if exited properly, non-zero otherwise
+ * main - prints the size of various types on the computer
+ * it is compiled and run on
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
