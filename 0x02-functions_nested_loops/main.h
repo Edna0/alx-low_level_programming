8afdbd7d5c4a6c095fe0de9386e1f761e14dbb5e
Edna0 <ojiedna@gmail.com>
2022-03-15 18:11:41 -0700

    Header File

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN
+#define MAIN
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+#endif
