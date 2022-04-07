c2e6c4cfe4ffbfb1df9293e83d9db2b101485e9f
Edna Oji <ojiedna@gmail.com>
2022-04-07 04:01:51 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,13 @@
 
 int main(int argc, char const *argv[])
 {
-	int i = 0;
-	
-	while (argc--)
-	{
-		printf("%s\n", argv[i]);
-		i++;
-	}	
-	return (0);
+int i = 0;
+
+while (argc--)
+{
+	printf("%s\n", argv[i]);
+	i++;
 }
+
+return (0);
+}	
