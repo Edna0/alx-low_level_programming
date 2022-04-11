f1ed4b9b1454bfe644e66c4bf2be0eeb6e88de4c
Edna Oji <ojiedna@gmail.com>
2022-04-11 16:09:48 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-  * main -  Prints the name of the file the program was compiled from
-  * Return - Always 0
+  * main - Prints the name of the file the program was compiled from
+  * Return: Always 0
   */
 
 int main(void)
