aa6c47a4d0b0cef3ce041dfeb157110e986088a3
Edna Oji <ojiedna@gmail.com>
2022-04-13 20:56:18 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,7 +8,6 @@
  * Return: pointer to the function that corresponds to the
  * operator given as a parameter
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,14 +20,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-    	i = 0;
+	i = 0;
 
-    	while (ops[i].op)
-    	{
+	while (ops[i].op)
+	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	
+
 	return (NULL);
 }
