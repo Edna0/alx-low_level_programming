00ca7a489a4fe167506c0c6bd22f77a1730a1966
Edna Oji <ojiedna@gmail.com>
2022-04-13 18:58:28 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * print_name_as_is - prints a name as is
+  * @name: name of the person
+  * @f: function pointer
+  * Return: Always 0
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
